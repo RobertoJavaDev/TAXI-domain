c6fec177fddaf174f5c164b32cf9af346b783781
RobertoJavaDev <robertojavadev@gmail.com>
2024-03-24 18:52:29 +0100

    Initial commit

---
+++ src/main/java/pl/robertojavadev/taxidomain/TaxiDomainApplication.java
@@ -0,0 +1,13 @@
+package pl.robertojavadev.taxidomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiDomainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/robertojavadev/taxidomain/TaxiDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.robertojavadev.taxidomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiDomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

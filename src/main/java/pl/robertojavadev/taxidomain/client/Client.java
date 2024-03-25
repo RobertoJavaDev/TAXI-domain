7686c93b23ac40d40b90e6976d74713c1d03f443
RobertoJavaDev <robertojavadev@gmail.com>
2024-03-25 20:26:33 +0100

    Added project structure

---
+++ src/main/java/pl/robertojavadev/taxidomain/client/Client.java
@@ -0,0 +1,25 @@
+package pl.robertojavadev.taxidomain.client;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+}
